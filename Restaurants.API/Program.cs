00bad5c5b181fcc80aadd215d2a42c14aec0da57
Ahmad Dalleh <112218109+AhmadDalleh@users.noreply.github.com>
2026-07-11 04:05:15 +0400

    Add Microsoft Identity and Configure the Authentication.

--- Restaurants.API/Program.cs
+++ Restaurants.API/Program.cs
@@ -1,27 +1,20 @@
-using Restaurants.API.Controllers;
 using Restaurants.API.Middlewares;
 using Restaurants.Application.Extensions;
+using Restaurants.Domain.Entities;
 using Restaurants.Infrastructure.Extensions;
 using Restaurants.Infrastructure.Seeders;
 using Serilog;
-using Serilog.Events;
-using System.Runtime.Serialization;
+using Restaurants.API.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
-builder.Services.AddSwaggerGen();
-builder.Services.AddScoped<ErrorHandlingMiddleware>();  
-
+builder.AddPresentation();
 builder.Services.AddApplication();
-
 builder.Services.AddInfrastructure(builder.Configuration);
 
-builder.Host.UseSerilog((context, configuration) =>
-    configuration.ReadFrom.Configuration(context.Configuration)
-);
+
 
 var app = builder.Build();
 
@@ -32,13 +25,18 @@ await seeder.Seed();
 
 app.UseMiddleware<ErrorHandlingMiddleware>();
 app.UseSerilogRequestLogging();
+
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
+app.MapGroup("/api/identity").MapIdentityApi<User>();
+
 app.UseHttpsRedirection();
+
 
 app.UseAuthorization();
 
