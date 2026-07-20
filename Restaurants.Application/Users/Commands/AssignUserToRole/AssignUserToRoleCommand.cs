a7e3c50b9b151b0d28f61a8edfe2deb122796aa8
Ahmad Dalleh <112218109+AhmadDalleh@users.noreply.github.com>
2026-07-21 00:28:01 +0400

    add assign and unassign roles end points

---
+++ Restaurants.Application/Users/Commands/AssignUserToRole/AssignUserToRoleCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace Restaurants.Application.Users.Commands.AssignUserToRole;
+
+public class AssignUserToRoleCommand : IRequest
+{
+    public string UserEmail { get; set; } = default!;
+    public string UserRole { get; set; } = default!;
+}
--- Restaurants.Infrastructure/Seeders/RestaurantSeeder.cs
+++ Restaurants.Infrastructure/Seeders/RestaurantSeeder.cs
@@ -33,9 +33,18 @@ internal class RestaurantSeeder(RestaurantDbContext dbContext) : IRestaurantSeed
     {
         List<IdentityRole> roles =
             [
-                new(UserRoles.User),
-                new(UserRoles.Owner),
+                new(UserRoles.User)
+                {
+                    NormalizedName = UserRoles.User.ToUpper()
+                },
+                new(UserRoles.Owner)
+                {
+                    NormalizedName = UserRoles.Owner.ToUpper()
+                },
                 new(UserRoles.Admin)
+                {
+                    NormalizedName = UserRoles.Admin.ToUpper()
+                }
             ];
         return roles;
     }
